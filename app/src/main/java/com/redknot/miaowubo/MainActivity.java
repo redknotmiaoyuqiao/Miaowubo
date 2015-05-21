5e039b61d357d01eca5ef1927790c29031172a57
YaoyaoloveQiaoqiao <redknotmiaoyuqiao@gmail.com>
2015-05-21 10:25:06 +0800

    hah

--- app/src/main/java/com/redknot/miaowubo/MainActivity.java
+++ app/src/main/java/com/redknot/miaowubo/MainActivity.java
@@ -135,7 +135,9 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_login) {
+            Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(intent);
             return true;
         }
 
